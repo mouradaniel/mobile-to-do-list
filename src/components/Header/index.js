4af6e47711d7d56f618bb85e612e7b4ad91d9660
Daniel Moura <daniel-moura@outlook.com>
2022-12-20 19:27:14 -0300

    feat: init header

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {
+  Container,
+  Content,
+  Title
+} from './styles';
+
+export function Header() {
+  return (
+    <Container>
+      <Title>To do list</Title>
+    </Container>
+  )
+}
---
+++ src/components/Header/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  background-color: #6AAAF7;
+  padding: 60px 24px 30px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+  color: #fff;
+  text-transform: uppercase;
+`;

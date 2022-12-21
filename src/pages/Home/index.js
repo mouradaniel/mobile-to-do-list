c4e93c0202a2baa962bbf01e20af21f102f6c78e
Daniel Moura <daniel-moura@outlook.com>
2022-12-21 18:35:08 -0300

    feat: implement tasks on home with fixed data

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Header } from '../../components/Header';
+import { Task } from '../../components/Task';
 
 import { 
-  Container
+  Container,
+  TaskList
 } from './styles';
 
 export function Home() {
+  const [tasks, setTasks] = useState([
+    {
+      id: "1",
+      category: "Personal",
+      description: "Organizing the next day's tasks",
+      priority: "High"
+    },
+    {
+      id: "2",
+      category: "Work",
+      description: "Re-read the book 'Clean Code'",
+      priority: "Low"
+    },
+    {
+      id: "3",
+      category: "Other",
+      description: "Going to the supermarket",
+      priority: "Medium"
+    }
+  ]);
+
   return (
     <Container>      
       <Header />
+
+      <TaskList>
+        {tasks.map((task) => {
+          return (
+            <Task key={task.id} task={task} />
+          )
+        })}
+      </TaskList>
     </Container>
   );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -4,3 +4,7 @@ export const Container = styled.SafeAreaView`
   flex: 1;
   background-color: #f0f0f0;
 `;
+
+export const TaskList = styled.View`
+  margin: 30px 0;
+`;
